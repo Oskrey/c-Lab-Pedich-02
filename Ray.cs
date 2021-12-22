c3191a9d7881058788cc7332c4e91e30c15a6a42
Никита <pedichn@gmail.com>
2021-12-22 19:34:03 +0300

    Vec2, Ray

--- Ray.cs
+++ Ray.cs
@@ -8,5 +8,17 @@ namespace Lab01
 {
     internal class Ray
     {
+        public Vec2 org;
+        public Vec2 dir;
+
+        public Ray(Vec2 org, Vec2 dir)
+        {
+            this.org = org;
+            this.dir = dir;
+        }
+        public Vec2 pointAtDistance(float t)
+        {
+            return org + dir * t;
+        }
     }
 }
